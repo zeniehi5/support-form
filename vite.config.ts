669838679ad4526050ec8ad4295c096e11042c9a
zeniehi5 <zeniehi5@gmail.com>
2025-05-02 13:57:31 -0700

    chore: fix base path in vite config for correct github pages routing

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/support-form/" : "/",
+  base: "/support-form/",
   plugins: [react(), tailwindcss()],
   css: {
     postcss: "./postcss.config.js",
