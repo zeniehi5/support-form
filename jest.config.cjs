7c3158f3e4d847890b8133f24dc9c12dceddbc05
zeniehi5 <zeniehi5@gmail.com>
2025-05-02 13:11:27 -0700

    chore(test): configure jest with ts-jest and jsdom for typescript testing

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+// jest.config.cjs
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  roots: ["<rootDir>/src"],
+};
